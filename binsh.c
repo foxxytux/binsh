2ded5c5a021b4962974688d238df7e8ad3eab6fd
FoxxyTux <foxxytux@proton.me>
2025-10-07 23:37:05 +0300

    Update binsh.c

--- binsh.c
+++ binsh.c
@@ -4,42 +4,45 @@
 #include <unistd.h>
 #include <linux/limits.h>
 #include <signal.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-// colors
 #define AC_YELLOW "\x1b[33m"
 #define AC_MAGENTA "\x1b[35m"
 #define AC_WHITE "\x1b[37m"
-#define AC_NORMAL "\x1b[m"
 
 char cmd[512];
 char fullsystem[1024];
-int system(const char *command);
-int flag=0;
-int empty=0;
 char cwd[PATH_MAX];
 
-int main() {
+int main( int argc, char *argv[] ) {
 	signal(SIGINT, SIG_IGN);
+	if ( argc > 1 ) {
+		FILE *file = fopen( argv[1], "r" );
+		if ( file == 0) { printf("File Does Not Exist!\n");	} else {
+			char line[256];
+			while (fgets(line, sizeof(line), file)) {
+				sprintf(fullsystem, "/bin/%s", line);
+				system(fullsystem); 
+			}
+		}
+	fclose(file);
+	return 0;
+	}
 	while (1) {
 		getcwd(cwd, sizeof(cwd));
-		printf("%s%s%s-%s%s%s$ ", AC_MAGENTA, "BinSH", AC_WHITE, AC_YELLOW, cwd, AC_NORMAL);
 		memset(cmd,0,512);
-		fgets(cmd, sizeof(cmd), stdin);
+		char prt[256];
+		sprintf(prt, "%s%s%s-%s%s%s$ ", AC_MAGENTA, "BinSH", AC_WHITE, AC_YELLOW, cwd, AC_WHITE);		
+		char *temp = readline(prt);
+		strncpy(cmd, temp, sizeof(cmd) - 1);
+		add_history(cmd);
+		free(temp);
 		cmd[strcspn(cmd, "\n")] = 0;
-		if (strcmp(cmd, "exit") == 0 || empty == 5) {
-			return 0;	
-		}
-		if(strlen(cmd) == 0) {signal(SIGINT, SIG_IGN);
-			empty++;
-			continue;
-		} else {
-			empty=0;
-		} 
-		if (strncmp(cmd, "cd ", 3) == 0) {
-			chdir(cmd + 3);
-		} else if (strncmp(cmd, "cd", 2) == 0){
-			chdir(getenv("HOME"));
-		} else {
+		if (strcmp(cmd, "exit") == 0) {	return 0; } 
+		if (strncmp(cmd, "cd ", 3) == 0) { chdir(cmd + 3); } 
+		else if (strncmp(cmd, "cd", 2) == 0){ chdir(getenv("HOME")); }
+		else {
 			sprintf(fullsystem, "/bin/%s", cmd);
 			system(fullsystem); 
 		}
